058bfb004d83e426890b3e4c24cd96cb0b2c0475
Angie Plazas <angie_plazas@epam.com>
2026-07-12 17:34:59 -0500

    clasificador de notas, el orden es importante, las más especificas primero

---
+++ ejercicios/clasificador-de-notas.js
@@ -0,0 +1,33 @@
+﻿/* Escribe una función llamada:
+
+clasificarNota(nota)
+
+Debe retornar:
+
+CondiciónRetorna
+nota >= 90"Excelente"
+nota >= 80"Muy bien"
+nota >= 70"Bien"
+nota >= 60"Aprobado"
+menor que 60"Reprobado"
+*/
+
+function clasificarNota(nota) {
+    if (nota >= 90) {
+        return "Excelente";
+    } else if (nota >= 80) {
+        return "Muy bien";
+    } else if (nota >= 70) {
+        return "Bien";
+    } else if (nota >= 60) {
+        return "Aprobado";
+    } else {
+        return "Reprobado";
+    }
+}
+
+console.log(clasificarNota(95));
+console.log(clasificarNota(84));
+console.log(clasificarNota(73));
+console.log(clasificarNota(60));
+console.log(clasificarNota(45));

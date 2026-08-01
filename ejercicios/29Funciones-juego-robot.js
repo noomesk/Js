8337f083e5fa9b86d9c74173e88d017d547850bb
noomesk <aplazasn@unal.edu.co>
2026-08-01 17:23:52 -0500

    Funciones reutilizables: juego del robot

---
+++ ejercicios/29Funciones-juego-robot.js
@@ -0,0 +1,40 @@
+// Define funciones para movimientos reutilizables (unidad funciones)
+// Ejemplo:
+
+function avanzarDerecha(pasos) {
+    robot.repeat(pasos,() => robot.moveRight());
+}
+
+avanzarDerecha(6);
+
+
+function avanzarAbajo (pasos) {
+    robot.repeat(pasos,() => robot.moveDown());
+}
+
+avanzarAbajo(2);
+
+
+function avanzarIzquierda (pasos) {
+    robot.repeat(pasos,() => robot.moveLeft());
+}
+
+avanzarIzquierda(6);
+
+function avanzarAbajo (pasos) {
+    robot.repeat(pasos,() => robot.moveDown());
+}
+
+avanzarAbajo(2);
+
+function avanzarDerecha(pasos) {
+    robot.repeat(pasos,() => robot.moveRight());
+}
+
+avanzarDerecha(6);
+
+function avanzarAbajo (pasos) {
+    robot.repeat(pasos,() => robot.moveDown());
+}
+
+avanzarAbajo(1);

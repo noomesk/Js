d41ba3e838e0e486ac6692f2359a6b82a3791a4c
noomesk <aplazasn@unal.edu.co>
2026-08-20 21:20:20 -0500

    Create 30arrayexercise.js

---
+++ ejercicios/30arrayexercise.js
@@ -0,0 +1,10 @@
+function primero(lista) {
+  return lista[0];
+}
+
+function ultimo(lista) {
+  return lista[lista.length - 1];
+}
+const numeros = [4, 8, 15];
+document.getElementById("salida").textContent =
+  "primero → " + primero(numeros) + " · ultimo → " + ultimo(numeros);

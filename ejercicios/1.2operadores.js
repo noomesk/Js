6fb6b449a0ae8cc18ec5f88b688c0ee709ec768a
noomesk <aplazasn@unal.edu.co>
2026-08-26 17:18:33 -0500

    ejercicio operadores

---
+++ ejercicios/1.2operadores.js
@@ -0,0 +1,12 @@
+// Las líneas que empiezan con // son notas.
+// JavaScript no las ejecuta.
+
+const numero = 8;
+const edad = 20;
+const tieneEntrada = true;
+
+const esPar = numero % 2 === 0;
+const puedeEntrar = edad >= 18 && tieneEntrada; 
+
+console.log(esPar);
+console.log(puedeEntrar);

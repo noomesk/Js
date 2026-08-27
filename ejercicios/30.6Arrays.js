76950c3f1f8107ce0d3718b83bea6185e403ea5a
noomesk <aplazasn@unal.edu.co>
2026-08-26 21:54:52 -0500

    Array: Lista de puntuaciones con índices

--- ejercicios/30.6Arrays.js
+++ ejercicios/30.6Arrays.js
@@ -2,12 +2,16 @@
 
 const puntuaciones = [10, 20, 30, 40, 50]; 
 /**posiciones:
- * índice 0 → 95
- * índice 1 → 82
- * índice 2 → 70
- * índice 3 → 64
+ *índice 0 → 10
+ *índice 1 → 20
+ *índice 2 → 30
+ *índice 3 → 40
+ *índice 4 → 50
  */
 console.log(puntuaciones[0]); // 10
-console.log(puntuaciones[3]); // 50
+console.log(puntuaciones[3]); // 40
 
-//indice: posición dentro deñl array
+//indice: posición dentro del array
+
+console.log(puntuaciones.length); //cuántos elementos tiene el array
+console.log(puntuaciones.length - 1); // último índice: 4

d33c1eeacca8b9e91cc9fb788ac8c045b9ec5a24
noomesk <aplazasn@unal.edu.co>
2026-08-27 00:17:13 -0500

    DOM: definiciones

---
+++ ejercicios/32.DOM.js
@@ -0,0 +1,9 @@
+// Las líneas que empiezan con // son notas.
+// JavaScript no las ejecuta.
+
+const titulo = document.getElementById("titulo"); //busca un elemento por su id
+const mensaje = document.getElementById("mensaje"); // document es la página
+
+// Cambia los dos textos originales.
+titulo.textContent = "Estoy entendiendo"; //esto puede cambiar el contenido original
+mensaje.textContent = "Jajaj esto es genial";

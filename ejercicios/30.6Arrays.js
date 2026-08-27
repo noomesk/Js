5a274b99492ba3e4e221d4be9b12aa9a91877907
noomesk <aplazasn@unal.edu.co>
2026-08-26 21:51:03 -0500

    Arrays: Representa una lista de puntuaciones con índices

---
+++ ejercicios/30.6Arrays.js
@@ -0,0 +1,13 @@
+//Representa una lista de puntuaciones con índices.
+
+const puntuaciones = [10, 20, 30, 40, 50]; 
+/**posiciones:
+ * índice 0 → 95
+ * índice 1 → 82
+ * índice 2 → 70
+ * índice 3 → 64
+ */
+console.log(puntuaciones[0]); // 10
+console.log(puntuaciones[3]); // 50
+
+//indice: posición dentro deñl array

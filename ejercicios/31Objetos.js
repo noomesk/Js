86c88eb4fae8b32a40a914af6f82f27fc3accb31
noomesk <aplazasn@unal.edu.co>
2026-08-26 22:55:01 -0500

    Objetos: ejercicio etiqueta(item)

---
+++ ejercicios/31Objetos.js
@@ -0,0 +1,18 @@
+/**
+ *  crea un objeto producto y elige los valores q quieras para
+ *  nombre y precio, después escribe etiqueta(item):
+ *  debe leer esos dos campos y devolver un texto con el formato
+ *  "Nombre - precio" debe funcionar con cualquier producto.
+ */
+
+
+const producto = {
+  nombre: "Angie",
+  precio: 250000,
+};
+
+function etiqueta(item) {
+  return item.nombre + " - " + item.precio; //item es el objeto q me pasen  ZB: etiqueta(producto) ES item = producto;; 
+}
+
+console.log(etiqueta(producto));

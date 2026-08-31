a1cbf041a9b52db9e7cf492ce041a13ceaea27d8
noomesk <aplazasn@unal.edu.co>
2026-08-31 15:29:17 -0500

    Fix: ejercicio actualizar cantidad

--- ejercicios/44estado.js
+++ ejercicios/44estado.js
@@ -1,3 +1,5 @@
+const { act } = require("react");
+
 //
 function enRango(valor, minimo, maximo) {
   return valor >= minimo && valor <= maximo;
@@ -15,3 +17,14 @@ console.log(actualizarCantidad(2, "sumar")); //texto del DOM
 //leer el número desde el DOM crea una segunda fuente de vdd
 //por otra parte puede cambiarlo y dejar la pantalla en desacuerdo
 //con el estado real
+
+//ejercicio:
+function actualizarCantidad(actual, accion) {
+  // La función debe producir el siguiente estado.
+  if (accion === "sumar") return actual + 1; 
+   if (actual > 0 && accion === "restar") return actual - 1;  // ojo con comparar dos veces lo mismo!
+   
+  return actual;
+}
+
+console.log(actualizarCantidad(2, "sumar"));   

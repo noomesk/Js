588a56b78a61a7d075d412066affcf96f6d54724
noomesk <aplazasn@unal.edu.co>
2026-09-11 09:31:20 -0500

    Ejercicio 1plataforma2: console-log

---
+++ ejercicios-plat.js/1console-log.js
@@ -0,0 +1,6 @@
+//Escribe tres mensajes que describan el inicio de tu día y 
+// predice el orden antes de ejecutar.
+
+console.log("1. levantarme");
+console.log("2. bañarme");
+console.log("3. ir a la universidad");

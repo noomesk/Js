361d67e159430c7d436aed853d60cb0a43f103f6
noomesk <aplazasn@unal.edu.co>
2026-09-11 09:47:49 -0500

    2. Valores, tipos y variabkes, ejercicio

---
+++ ejercicios-plat.js/2valores-tipos-variables.js
@@ -0,0 +1,10 @@
+// Crea un texto, un número que pueda cambiar y un booleano.
+
+const nombre = "Angie";
+let nivel = 9; 
+let disponible = true;
+
+nivel = 6;
+console.log(nombre);
+console.log(nivel);
+console.log(disponible); 

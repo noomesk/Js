5049fd7a962255383fa215e9813839e34ac0c45a
noomesk <aplazasn@unal.edu.co>
2026-09-11 11:05:19 -0500

    3.Operadores y conversiones

---
+++ ejercicios-plat.js/3Op-y-conversiones.js
@@ -0,0 +1,16 @@
+const minutosEscritos = "8";
+// Sustituye el texto, convierte y calcula cuántos segundos son.
+const numero = Number(minutosEscritos);
+const resultado = (numero * 60); 
+console.log(resultado);
+
+//ejercicio de contador: (para q cambie hay q usar let no const)
+let contador = 1;
+contador = 2;
+console.log(contador);
+
+
+//Number transforma texto en num: 
+const edad = Number("20");
+const puedeEntrar = edad >= 18;
+console.log(puedeEntrar);
